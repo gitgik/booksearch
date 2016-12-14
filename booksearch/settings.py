87a49c0d4b0c144c15eff97a32d05500a5e7e34c
Jee Gikera <githinjigikera@gmail.com>
2016-12-14 16:47:47 +0300

    (chore) Add templates folder to be recognized on settings

--- booksearch/settings.py
+++ booksearch/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'is$%&yk6g__iamo6#=5b^q)&1=ibc9!g%geszo%myk_)iy5xk-'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'search',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'booksearch.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -75,8 +76,10 @@ WSGI_APPLICATION = 'booksearch.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'booksearch',
+        'HOST': 'localhost',
+        'USER': 'nerd',
     }
 }
 
