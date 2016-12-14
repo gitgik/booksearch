1c85e41c233c687e1966eac45a92ba0202ad33eb
Jee Gikera <githinjigikera@gmail.com>
2016-12-14 19:37:58 +0300

    (chore) Refactor form to include category field

--- search/forms.py
+++ search/forms.py
@@ -6,13 +6,12 @@ class SearchForm(ModelForm):
     """This class defines a form."""
 
     CHOICES = [('title', 'Title'), ('category', 'Category')]
-    choice = ChoiceField(choices=CHOICES, label="Search by ")
 
     class Meta:
         """Meta class to define model and associated field for the form."""
 
         model = Book
-        fields = ['title']
+        fields = ['title', 'category']
 
 
 class CategoryForm(ModelForm):
