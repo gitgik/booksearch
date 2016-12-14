54fbac7c4380f126b98a8f15a58724b387214f58
Jee Gikera <githinjigikera@gmail.com>
2016-12-14 16:55:58 +0300

    (feature) Create a search form for querying books by title and category

---
+++ search/forms.py
@@ -0,0 +1,26 @@
+from django.forms import ModelForm, ChoiceField, CharField
+from .models import Book, BookCategory
+
+
+class SearchForm(ModelForm):
+    """This class defines a form."""
+
+    CHOICES = [('title', 'Title'), ('category', 'Category')]
+    choice = ChoiceField(choices=CHOICES, label="Search by ")
+    category = CharField(required=False)
+
+    class Meta:
+        """Meta class to define model and associated field for the form."""
+
+        model = Book
+        fields = ['title', 'category']
+
+
+
+class CategoryForm(ModelForm):
+    """This class defines a categroy form."""
+    class Meta:
+        """Meta class to define model and associated field for the form."""
+
+        model = BookCategory
+        fields = ['name', ]
